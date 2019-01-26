cfe0b69873e8d3cd778206cc3aa8994ad06c578a
DaiTianyu <dtysky@outlook.com>
2019-01-27 01:33:07 +0800

    Update

---
+++ src/assets/convert.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+import os, json
+from PIL import Image
+
+IMAGES = []
+
+for root, dirs, files in os.walk('./original'):
+    for f in files:
+      name, ext = os.path.splitext(f)
+      if ext == '.png':
+        IMAGES.append((root.replace('/original', '/') + f, Image.open(root + '/' + f)))
+
+for entity in IMAGES:
+  path = entity[0]
+  image = entity[1]
+  (w, h) = image.size
+  image = image.resize((int(w / 5), int(h / 5)))
+  image.save(path)
