858465dc63d38754b2ef72a3eb42f595ddeac934
t <t@t.com>
2019-01-26 04:08:29 -0500

    WIP: level1

--- src/pages/Level1/index.tsx
+++ src/pages/Level1/index.tsx
@@ -6,19 +6,91 @@ import {
 import assets from '../../assets';
 
 interface IPropTypes extends RouteComponentProps<{sub: string}> {
-
 }
 
 interface IStateTypes {
-
+  items: {[name:string]: {
+      zIndex: number,
+      ref: React.Ref<any>,
+      x: number,
+      y: number,
+    },
+  },
 }
 
 class Level1 extends React.Component<IPropTypes, IStateTypes> {
+  state = {
+    items: {
+      gameboy: {
+        zIndex: 100,
+        x: 30,
+        y: 100,
+        ref: React.createRef(),
+      },
+      book: {
+        zIndex: 101,
+        x: 60,
+        y: 200,
+        ref: React.createRef(),
+      }
+    }
+  }
   public async componentDidMount() {
     const subLevel = this.props.match.params.sub;
   }
+  isDragging = false;
+  mouseDownX;
+  mouseDownY;
+  cachedX;
+  cachedY;
+  draggingItem;
+  onMouseMove = (e) => {
+    if (!this.isDragging) {
+      return;
+    }
+    const x = e.pageX;
+    const y = e.pageY;
+    const diffX = x - this.mouseDownX;
+    const diffY = y - this.mouseDownY;
+    const items = this.state.items;
+    items[this.draggingItem].x = this.cachedX + diffX + 'px';
+    items[this.draggingItem].y = this.cachedY + diffY + 'px';
+    Object.keys(items).forEach(i => items[i].zIndex--);
+    items[this.draggingItem].zIndex = 100 + Object.keys(items).length - 1;
+    this.forceUpdate();
+  }
+  onMouseUp= (e) => {
+    this.isDragging = false;
+  }
+  itemOnMouseDown = (e, itemName) => {
+    this.isDragging = true;
+    this.mouseDownX = e.pageX;
+    this.mouseDownY = e.pageY;
+    this.draggingItem = itemName;
+    const ref = this.state.items[itemName].ref;
+    this.cachedX = parseInt(ref.current.style.left);
+    this.cachedY = parseInt(ref.current.style.top);
+  }
   public render() {
-    return null;
+    return <div
+      className="bg"
+      onMouseMove={this.onMouseMove}
+      onMouseUp={this.onMouseUp}
+    >
+      {
+        Object.keys(this.state.items).map(i => <div
+          className={i}
+          ref={this.state.items[i].ref}
+          style={{
+            position: 'absolute',
+            left: this.state.items[i].x,
+            zIndex: this.state.items[i].zIndex,
+            top: this.state.items[i].y,
+          }}
+          onMouseDown={(e) => this.itemOnMouseDown(e, i)}
+        />)
+      }
+    </div>;
   }
 }
 
--- src/pages/Title.tsx
+++ src/pages/Title.tsx
@@ -5,6 +5,7 @@
  * @Date   : 1/26/2019, 11:31:10 AM
  */
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 import assets from '../assets';
 
@@ -18,6 +19,8 @@ interface IStateTypes {
 
 export default class Title extends React.PureComponent<IPropTypes, IStateTypes> {
   public render() {
-    return <img src={assets.getSrc('test')} />;
+    return <Link to="/level/1/1">
+      <img src={assets.getSrc('test')} />
+    </Link>;
   }
 }
